c9c7ca75e5c8cf67d8e55118a3ace962cff1e06b
abdullahbader <abdallah.b.b96@gmail.com>
2021-05-31 14:15:41 +0300

    late

--- cards.js
+++ cards.js
@@ -1,69 +1,94 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-Array.prototype.getRandom = function () {
-    return this.slice(Math.floor(Math.random() * this.length), 1)[0];
-}
-
 class Node {
-    constructor(color,number,nextnode = null){
-    this.color = color
-    this.number= number
-    this.nextnode=nextnode
-
+    constructor(color, number, nextNode = null) {
+      this.color = color;
+      this.number = number;
+      this.nextNode = nextNode;
     }
-}
-
-
-class Stack{
-
-    constructor(limit = 20){
-    this.limit = limit
-    this.size  =   0
-    this.topnode= null
-    this.nextnode= null
-
+    getData = () => {
+      return `${this.color} - ${this.number}`;
+    };
+  }
+  
+  class Stack {
+    constructor(limit = 20) {
+      this.topNode = null;
+      this.limit = limit;
+      this.size = 0;
     }
-
-    push = (color,number)=>{
-        if(this.size === this.limit){
-        console.log("stack is full")}
-        else{
-        const newNode = new Node(newraan)
-        newNode.nextnode= this.topnode
-        this.topnode = newNode
-        this.size++
-        }
+  
+    isEmpty = () => this.size === 0;
+  
+    isFull = () => this.size === this.limit;
+    peek = () => this.topNode.getData();
+  
+    pop = () => {
+      if (this.isEmpty()) return "still no cards yet !!";
+      else {
+        const removedTop = this.topNode;
+        this.topNode = removedTop.nextNode;
+        this.size--;
+        return removedTop.getData();
+      }
+    };
+    push = (color, number) => {
+      if (this.isFull()) console.log("max card limit");
+      else {
+        const newNode = new Node(color, number);
+        newNode.nextNode = this.topNode;
+        this.topNode = newNode;
+        this.size++;
+        //return this.topNode.getData();
+      }
+    };
+    shuffledCards = () => {
+      let currnetNode = this.topNode;
+      while (currnetNode) {
+        console.log(
+          `color : ${currnetNode.color} - number : ${currnetNode.number}`
+        );
+        currnetNode = currnetNode.nextNode;
+      }
+    };
+  }
+  
+  let colors = ["blue", "yellow", "red", "green"];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  
+  const random = (array) => {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+  };
+  
+  const cards = new Stack(20);
+  const cardsArray = [];
+  
+  while (!cards.isFull()) {
+    const color = random(colors);
+    const number = random(numbers);
+    if (!cardsArray.includes(`${color}, ${number}`)) {
+      cards.push(color, number);
+      cardsArray.push(`${color}, ${number}`);
     }
-
-    pop = (data) => {
-        if(this.size === 0){
-        return "stack is empty "
-        }        
-        else
-        while(this.size< 5 ){}
-        const poped = this.topnode
-        const player1= player1.push[poped]
-        this.topnode = poped.nextnode
-        this.size--
-        return poped.data
-
-    }
-
-
-    get peek() {
-        return this.topNode.data;
-
-}
-}
-
-
-const newraan= raan([red,blue,yellow,green],[1,2,3,4,5,6,7,8,9]) 
-
-
-const raan = (color,number)=>{
-    let i = 0;
-    while (i < 1){
-      newcards.push([color.getRandom(), number.getRandom()]);
-     i++ }
-    return newcards;
-}
+  }
+  
+  //while (topNode.color !== color && topNode.number !== number) {
+  // return cards.push(color, number);
+  //}
+  
+  //console.log("deck :", cards);
+  
+  //console.log(`color : ${color} - number : ${number}`);
+  cards.shuffledCards();
+  
+  let player1 = [];
+  let player2 = [];
+  
+  let i = 0;
+  while (i < 5) {
+    player1.push(cards.pop());
+    player2.push(cards.pop());
+    i++;
+  }
+  console.log(`player1 : ${player1}`);
+  console.log(`player2 : ${player2}`);
+  console.log(cards.peek());
