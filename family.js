34f5406b6fffd239b27e0787dd702213a291095a
abdullahbader <abdallah.b.b96@gmail.com>
2021-05-31 16:38:43 +0300

    zainad so

--- family.js
+++ family.js
@@ -1,48 +1,53 @@
-const prompt = require('prompt-sync')({sigint: true});
+  
+const prompt = require("prompt-sync")({ sigint: true });
 
-class Node {
-constructor(name){
-this.name=name
-this.children=[]
-}
-}
+class TreeNode {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
 
-class Tree { 
-    constructor(name){
-    this.name=name
-    this.children=[]
+  addChild = (node) => {
+    if (this.children.length < 2) {
+      this.children.push(node);
+      console.log(`child ${child.name}`);
+    } else console.log("orphrn");
+  };
 
+  //removeChild = (node) => this.children.filter((child) => child !== node);
+
+  traverse = () => {
+    let nodes = [this];
+    while (nodes.length > 0) {
+      let currentNode = nodes.pop();
+      console.log(currentNode.name);
+      nodes = [...nodes, ...currentNode.children];
     }
+  };
 
-    addchild=(child) => {
-        if(this.children.length < 3)
-        this.children.push(child)
+  search = (child) => {
+    let nodes = [this];
+    while (nodes.length > 0) {
+      let currentNode = nodes.pop();
+      if (child.name.split(" ")[1] === currentNode.name.split(" ")[0]) {
+        return currentNode;
+      }
+      nodes = [...nodes, ...currentNode.children];
     }
-
-    traverse = () => {
-       let firstName = prompt("please enter your child name :")
-       let parentName =  prompt("please enter your parent name :")
-     
-       if(parentName === this.name)
-       this.addchild(firstName)
-       else console.log("parent not found")
-       
-     //   let nodes = [this]; // to put the root node in our array
-     //   while (nodes.length > 0) {
-     //     let currentNode = nodes.pop();
-     //     console.log(currentNode.data);
-     //     nodes = [...nodes, ...currentNode.children];
-     //  }
-      ;
-
+    return " no parent";
+  };
 }
+let childName = prompt(`please enter child full name (done if finished)`);
+let root = new TreeNode("family");
+
+while (childName !== "done") {
+  let tree = new TreeNode(childName);
+  let parent = root.search(tree);
+
+  if (parent !== " no parent") {
+    parent.addChild(tree);
+  } else console.log(parent);
+  childName = prompt(`please enter child full name (done if finished)`);
 }
-
-
-const root = new Tree("family")
-// cName = prompt("please enter your child name :")
-
-
-
-root.traverse()
-console.log(root.children)
+//if (childName === "done") tree.traverse;
+root.traverse();
