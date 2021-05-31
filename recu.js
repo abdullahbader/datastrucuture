2f73fed8af9fddd5e44b4bba9ea67444b5c9c0bf
abdullahbader <abdallah.b.b96@gmail.com>
2021-05-31 16:43:07 +0300

    hash

--- recu.js
+++ recu.js
@@ -1,13 +1,15 @@
-const numbers = [ [1, 2, 3, 4], [3, 6, [5, 6], 8, 2,[2, 4], 9], [4, 2, [6, 7, [2, 6, 1]]]]
-
-
-const sum = (group) => {
-    let total = 0
-    group.map((element) =>{
-        if(!isNaN(element))
-        total += element
-        else total +=sum(element)
-    })   
-   return total
-}
-
+const numbers = [
+    [1, 2, 3, 4],
+    [3, 6, [5, 6], 8, 2, [2, 4], 9],
+    [4, 2, [6, 7, [2, 6, 1]]],
+  ];
+  
+  const Sum = (numbers) => {
+    let sum = 0;
+    numbers.map((number) => {
+      if (typeof number === "number") sum += number;
+      else sum += Sum(number);
+    });
+    return sum;
+  };
+  console.log(Sum(numbers));
